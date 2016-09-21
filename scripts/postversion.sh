f44f0b6b65ccd30dcc9892aeed39b9ac06a5d28c
Imogen <imogen.wentworth@gmail.com>
2016-09-20 23:31:47 -0700

    Add extra publish scripts to postversion

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -7,3 +7,9 @@ git commit -m $(node -p -e "require('./package.json').version")
 
 echo "-- Pushing to gh-pages"
 git subtree push --prefix dist origin gh-pages
+
+echo "-- Pushing to master"
+git push origin master
+
+echo "-- Publishing on npm"
+npm publish
